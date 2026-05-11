9078dcc4c92a1c027b809247473235db1d324a70
JorgeVergar4 <jor.vergaras@duocuc.cl>
2026-05-11 12:37:50 -0400

    Pipeline test 3 back

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2/promise");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-// Configuración de la base de datos con variables de entorno testeoo
+// Configuración de la base de datos con variables de entorno testeo
 const {
   DB_HOST = "10.0.2.160", // acá colocar la IP Privada EC2 DB
   DB_USER = "alumno",
